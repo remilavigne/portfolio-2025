1dd98d5c19ecb111146550d8ce2416e323deee91
Remi Lavigne <remilavigne.pro@gmail.com>
2024-10-15 11:31:03 +0200

    button component created

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,115 +1,23 @@
-import { useState, useEffect } from "react";
-import { Link } from "react-scroll";
-import DownloadButton from './DownloadButton';
-import Buttton from '../../Components/Button';
-
-function Navbar() {
-  const [navActive, setNavActive] = useState(false);
-
-  const toggleNav = () => {
-    setNavActive(!navActive);
-  };
-
-  const closeMenu = () => {
-    setNavActive(false);
-  };
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth <= 500) {
-        closeMenu();
-      }
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  useEffect(() => {
-    if (window.innerWidth <= 1200) {
-      closeMenu();
-    }
-  }, []);
-
+export default function Navbar() {
   return (
-    <nav className={`navbar ${navActive ? "active" : ""}`}>
+    <nav>
       <div>
-        <img src="./img/logo.svg" alt="Logoipsum" />
+        <h1>LOGO</h1>
       </div>
-      <button
-        className={`nav__hamburger ${navActive ? "active" : ""}`}
-        onClick={toggleNav}
-      >
-        <span className="nav__hamburger__line"></span>
-        <span className="nav__hamburger__line"></span>
-        <span className="nav__hamburger__line"></span>
-      </button>
-      <div className={`navbar--items ${navActive ? "active" : ""}`}>
         <ul>
-          <li>
-            <Link
-              onClick={closeMenu}
-              activeClass="navbar--active-content"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-              to="heroSection"
-              className="navbar--content"
-            >
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              onClick={closeMenu}
-              activeClass="navbar--active-content"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-              to="MyPortfolio"
-              className="navbar--content"
-            >
-              Portfolio
-            </Link>
-          </li>
-          <li>
-            <Link
-              onClick={closeMenu}
-              activeClass="navbar--active-content"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-              to="AboutMe"
-              className="navbar--content"
-            >
-              About Me
-            </Link>
-          </li>
-          <li>
-            <Link
-              onClick={closeMenu}
-              activeClass="navbar--active-content"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-              to="Contact"
-              className="navbar--content"
-            >
-              Contact
-            </Link>
-          </li>
+            <li>
+                <a href="#home">Home</a>
+            </li>
+            <li>
+                <a href="#about">About</a>
+            </li>
+            <li>
+                <a href="#services">Services</a>
+            </li>
+            <li>
+                <a href="#contact">Contact</a>
+            </li>
         </ul>
-      </div>
-      <DownloadButton />
     </nav>
   );
 }
-
-export default Navbar;
--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Navbar />
       <h1>
         Hello there 👋 <br />
         I'm Rémi, a front-end web developer, passionate about UX, interactions and pixel perfect design.</h1>
