62834dcd8df5737423b5d207a51aa003ccb83575
Remi Lavigne <remilavigne.pro@gmail.com>
2024-10-15 21:09:50 +0200

    nabvar content updated, need work on className

--- app/page.jsx
+++ app/page.jsx
@@ -3,11 +3,11 @@ import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <>
       <Navbar />
-      <h1>
+      <h1 className="">
         Hello there 👋 <br />
         I'm Rémi, a front-end web developer, passionate about UX, interactions and pixel perfect design.</h1>
-    </div>
+    </>
   );
 }
